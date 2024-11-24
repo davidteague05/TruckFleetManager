15480d46abf1c0be1f9a310e02ac7ed5b5861c99
teagu <dmteague@lakeheadu.ca>
2024-11-24 15:49:43 -0500

    added authorization and authentication. required to signin to view anything other than details

--- TruckFleetManager/Controllers/TruckTypesController.cs
+++ TruckFleetManager/Controllers/TruckTypesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using TruckFleetManager.Models;
 
 namespace TruckFleetManager.Controllers
 {
+    [Authorize]
     public class TruckTypesController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -26,6 +28,7 @@ namespace TruckFleetManager.Controllers
         }
 
         // GET: TruckTypes/Details/5
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
--- TruckFleetManager/Controllers/TrucksController.cs
+++ TruckFleetManager/Controllers/TrucksController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TruckFleetManager.Data;
 using TruckFleetManager.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TruckFleetManager.Controllers
 {
+    [Authorize]
     public class TrucksController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -27,6 +29,7 @@ namespace TruckFleetManager.Controllers
         }
 
         // GET: Trucks/Details/5
+        [AllowAnonymous]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
