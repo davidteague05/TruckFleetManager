a3996a3f855e0fad32430fb301b4cf813087805e
teagu <dmteague@lakeheadu.ca>
2024-11-24 21:09:50 -0500

    added some css, a logo, and got the view trucks page working along with being able to login with google and website account

---
+++ TruckFleetManager/Controllers/CompanyTrucksController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TruckFleetManager.Data;
+
+namespace TruckFleetManager.Controllers
+{
+    public class CompanyTrucksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CompanyTrucksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var truckTypes = _context.Type.ToList();
+            return View(truckTypes);
+        }
+
+        public IActionResult ByTruckType(int id)
+        {
+            if (id == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var trucks = _context.Truck.Where(t => t.TruckId == id).ToList();
+
+            return View(trucks);
+        }
+    }
+}
